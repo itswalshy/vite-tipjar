aed2d0a2164bd541b74e886ca3725f7d0b9fc313
Your Name <your.email@example.com>
2025-05-02 12:01:14 -0400

    Update config for sbux.tips/test-tipjar/ deployment

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,7 +1,7 @@
 export default {
   title: "TipJar",
   description: "Tip calculation application for partners",
-  base: '/vite-tipjar/',
+  base: '/test-tipjar/',
   outDir: 'docs/.vitepress/dist',
   themeConfig: {
     // Removing navigation and sidebar for a single page app
